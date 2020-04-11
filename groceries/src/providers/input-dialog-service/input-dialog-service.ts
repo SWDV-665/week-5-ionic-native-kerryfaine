6532d51889c633485d6c48949e398a9aebc7e9ea
kerryfaine <46699746+kerryfaine@users.noreply.github.com>
2020-04-11 17:35:15 -0500

    week 5 added cordova library
    added a social sharing feature using Cordova

---
+++ groceries/src/providers/input-dialog-service/input-dialog-service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { GroceriesServiceProvider } from '../../providers/groceries-service/groceries-service';
+import { AlertController } from 'ionic-angular';
+
+/*
+  Generated class for the InputDialogServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class InputDialogServiceProvider {
+
+  constructor(public alertCtrl: AlertController, public dataService: GroceriesServiceProvider) {
+    console.log('Hello InputDialogServiceProvider Provider');
+  }
+
+  showPrompt(item, index?) {
+    const prompt = this.alertCtrl.create({
+      title: item ? 'Edit Item' : 'Add Item',
+      message: item ? "Please edit item..." : "Please ednter item",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name',
+          value: item ? item.name : null
+        },
+        {
+          name: 'qty',
+          placeholder: 'Quantity',
+          value: item ? item.qty : null
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Save clicked', item);
+
+            if (index !== undefined) {
+              this.dataService.editItem(item, index);
+            }
+            else {
+              this.dataService.addItem(item);
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+}
